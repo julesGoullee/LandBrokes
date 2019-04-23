3b6e71785fb6ce4c63667ea7f402b34a4cdfa65e
jules goullée <julesgoullee@gmail.com>
2019-04-23 15:38:27 +0200

    Backend (#6)
    * bidForLand && finBestLands
    
    * debugger
    
    * bank buyLand, checkBidForLands
    
    * lint
    
    * decentralandMarketplaceContract address
    
    * compile
    
    * ganache-cli dept

---
+++ backend/config/index.js
@@ -0,0 +1,19 @@
+require('dotenv').config();
+
+const Config = {
+  tokenAddress: process.env.TOKEN_ADDRESS || '0x0000000000000000000000000000000000000000000000000000000000000000',
+  privateKey: process.env.PRIVATE_KEY || '0x0123456789012345678901234567890123456789012345678901234567890123',
+  infuraApiKey: process.env.INFURA_KEY || null,
+  network: process.env.NETWORK || 'ropsten',
+  contractsAddress: {
+    ropsten: {
+      addressContractBank: process.env.ADDRESS_BANK || '0x0',
+      decentralandBidContract: process.env.DECENTRALAND_BID_CONTRACT || '0x0',
+      decentralandMarketplaceContract: process.env.DECENTRALAND_MARLETPLACE_CONTRACT || '0x0',
+      decentralandMarket: 'https://market.decentraland.zone',
+      decentralandApi: 'https://api.decentraland.zone'
+    }
+  }
+};
+
+module.exports = Config;
